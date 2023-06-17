8350415989cb53792fcd2625d917eaa756ef1f29
endangsalmadevi <endangsalmadevi@gmail.com>
2023-06-17 12:04:36 +0700

    Konfigurasi Project

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '643ea575d1c94237ffbb';
+  static const String projectId = '6429382e4ffb64619212';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
